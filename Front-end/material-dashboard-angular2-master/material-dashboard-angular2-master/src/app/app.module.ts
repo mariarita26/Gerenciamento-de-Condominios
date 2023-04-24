d0bbac58375e349789e2278e38aa991c41f69b56
Maria Rita Lira Vieira de Melo <maria.melo@polodeinovacao.ifpb.edu.br>
2023-04-23 22:39:08 -0300

    criação das interfaces e crud residente

--- Front-end/material-dashboard-angular2-master/material-dashboard-angular2-master/src/app/app.module.ts
+++ Front-end/material-dashboard-angular2-master/material-dashboard-angular2-master/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ResidenteModule } from './residente/residente.module';
 
 @NgModule({
   imports: [
@@ -17,6 +18,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
+    ResidenteModule
   ],
   declarations: [
     AppComponent,
