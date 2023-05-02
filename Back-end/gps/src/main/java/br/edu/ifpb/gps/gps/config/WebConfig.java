166f252092d14f790da2171c3ae60bea7c6d46f1
Pedro Paulo Cantalice <88915142+PedroTsI@users.noreply.github.com>
2023-05-02 11:17:28 -0300

    Crud visitantes (#15)
    * Update
    
    * Update Cors Config
    
    * Update Cors Config
    
    ---------
    
    Co-authored-by: Pedro Paulo Cantalice Marinho Braga <pedro.cantalice@assert.ifpb.edu.br>

---
+++ Back-end/gps/src/main/java/br/edu/ifpb/gps/gps/config/WebConfig.java
@@ -0,0 +1,10 @@
+package br.edu.ifpb.gps.gps.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedHeaders("*");
+    }
+}
