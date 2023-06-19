9a3c40e990ac867e10647579626b8749c0b132a0
Pedro Paulo Cantalice Marinho Braga <pedro.cantalice@assert.ifpb.edu.br>
2023-06-19 13:19:32 -0300

    Update

--- Front-end/material-dashboard-angular2-master/material-dashboard-angular2-master/src/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyCBudz46uaKDyyRA9E67tYF74ZV_aOciKI",
-    authDomain: "fortseg-c001f.firebaseapp.com",
-    projectId: "fortseg-c001f",
-    storageBucket: "fortseg-c001f.appspot.com",
-    messagingSenderId: "706609841093",
-    appId: "1:706609841093:web:b1a89410d825f60290c1ac"
-  };
