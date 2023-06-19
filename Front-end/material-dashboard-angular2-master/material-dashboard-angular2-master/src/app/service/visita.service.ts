c94452e66459000a55dd8f33f59e6355b8b15386
Pedro Paulo Cantalice Marinho Braga <pedro.cantalice@assert.ifpb.edu.br>
2023-06-19 12:00:33 -0300

    visita firestore

--- Front-end/material-dashboard-angular2-master/material-dashboard-angular2-master/src/app/service/visita.service.ts
+++ Front-end/material-dashboard-angular2-master/material-dashboard-angular2-master/src/app/service/visita.service.ts
@@ -1,38 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Visita } from 'app/interface/visita';
 import { environment } from 'environments/environment';
-import { Observable, catchError } from 'rxjs';
+import { Observable, catchError, from } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class VisitaService {
+  NOME_COLECAO = 'visita';
+  colecaoVisita: AngularFirestoreCollection<Visita>;
 
-  private readonly URL_visitas = `${environment.API}/visita`;
+  private readonly URL_visitas = `${environment.API}/porteiro/visita`;
 
-  constructor(private http: HttpClient) { }
-
-  registrarVisita(visita: Visita): Observable<any>{
-      return this.http.post<any>(this.URL_visitas, visita);
+  constructor(private afs: AngularFirestore) {
+    this.colecaoVisita = afs.collection(this.NOME_COLECAO);
   }
 
-  getAll(): Observable<Visita[]>{
-    return this.http.get<Visita[]>(this.URL_visitas);
+  registrarVisita(visita: Visita): Observable<object>{
+      delete visita.idVisita;
+      return from(this.colecaoVisita.add(Object.assign({}, visita)));
+  }
+
+  getAll(){
+    return this.colecaoVisita.valueChanges({idField: 'id'});
   } 
 
-  getVisitasByVisitanteId(visitanteId: number): Observable<Visita[]> {
-    return this.http.get<Visita[]>(`${environment.API}/visitantes/${visitanteId}/visitas`)
-      .pipe(
-        catchError((error: any) => {
-          console.error('Erro ao obter visitas do visitante:', error);
-          throw error;
-        })
-      );
+  getVisitasByVisitanteId(visitanteId: string) {
+    return this.colecaoVisita.doc(visitanteId).get();
   }
 
-  getVisitasPorPorteiro(idPorteiro: string): Observable<Visita[]> {
-    const url = `${environment.API}?porteiroId=${idPorteiro}`;
-    return this.http.get<Visita[]>(url);
-  }
+  // getVisitasPorPorteiro(idPorteiro: string): Observable<Visita[]> {
+  //   const url = `${environment.API}?porteiroId=${idPorteiro}`;
+  //   return this.http.get<Visita[]>(url);
+  // }
 }
